09eeacaf9238968fa6ad9784256340f8c5b283a7
romeo <romeo@listcloud.cn>
2017-12-22 16:41:44 +0800

    1、代码优化 2、关机时关闭gpio pin脚本

---
+++ gpio_poweroff.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#利用echo输出一些提示语句
+echo export pin $1 $2
+echo $1 > /sys/class/gpio/export
+
+echo setting direction to output
+echo out > /sys/class/gpio/gpio$1/direction
+
+echo setting pi
+echo $2 > /sys/class/gpio/gpio$1/value
